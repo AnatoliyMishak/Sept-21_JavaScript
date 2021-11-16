1f436fff6355c3edea153a45b4146d6972abf3ca
AnatoliyMishak <mishak_tolja@ukr.net>
2021-11-16 18:30:45 +0200

    HW JS Lesson3

--- Lesson3/Homework3/hw3.js
+++ Lesson3/Homework3/hw3.js
@@ -1,17 +1,6 @@
 
 
-<!-- - Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.-->
-<!-- - Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.-->
-<!-- - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи-->
-<!-- - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи-->
-<!-- - Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи-->
 
-<!-- - Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів. Вивести в консоль всі його елементи в циклі.-->
-<!-- - Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write-->
-<!-- - Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write-->
-<!-- - Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write-->
-<!-- - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write-->
-<!-- - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write-->
 
 
 // --створити масив з:
@@ -77,7 +66,115 @@
 
 <!-- - Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.-->
 
-let array = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111];
-for (let t=0; t<10; t++){
-    console.log(`${array[t]}`)
-}
+// let array = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111];
+// for (let t=0; t<10; t++){
+//     console.log(array[t])}
+//
+
+<!-- - Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.-->
+
+// let array = ['ko toto', 'na nya pozeratj', 'z obrazchyka', 'na stini', 'to nash uyko', 'to ye lenin', 'vun', 'takyj', 'blyzkyy', 'mini'];
+// for (let t=0; t<10; t++){
+//     console.log(array[t])
+// }
+
+<!-- - Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.-->
+
+// let array = [26, true, 'z obrazchyka', 34, 43, false, 'vun', 76, true, 'mini'];
+// for (let t=0; t<10; t++){
+//     console.log(`${array[t]}`)
+// }
+
+<!-- - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи-->
+<!-- - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи-->
+<!-- - Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи-->
+
+// let array = [26, true, 'z obrazchyka', 34, 43, false, 'vun', 76, true, 'mini'];
+// for (let t=0; t<array.length; t++){
+//     if (typeof array[t] === 'boolean'){
+//     console.log(array[t])
+//             }
+// }
+
+// let array = [26, true, 'z obrazchyka', 34, 43, false, 'vun', 76, true, 'mini'];
+// for (let t=0; t<array.length; t++){
+//     if (typeof array[t] === 'string'){
+//         console.log(array[t])
+//     }
+// }
+
+// let array = [26, true, 'z obrazchyka', 34, 43, false, 'vun', 76, true, 'mini'];
+// for (let t=0; t<array.length; t++){
+//     if (typeof array[t] === 'number'){
+//         console.log(array[t])
+//     }
+// }
+
+<!-- - Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів. Вивести в консоль всі його елементи в циклі.-->
+
+// let lastarray = [];
+// lastarray[0] = true;
+// lastarray[1] = 26;
+// lastarray[2] = 'name';
+// lastarray[3] = 'work';
+// lastarray[4] = false;
+// lastarray[5] = true;
+// lastarray[6] = 11;
+// lastarray[7] = 'home';
+// lastarray[8] = 1984;
+// lastarray[9] = 'dog';
+//
+// for(t=0; t<lastarray.length; t++)
+// {
+//     console.log(lastarray[t])
+// }
+
+<!-- - Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write-->
+
+// // for (let t=0; t<10; t++){
+// //     console.log('current step '+t+'')
+// document.write(`<div>`)
+// document.write('current step '+t+'')
+// document.write(`</div>`)
+//
+// // }
+<!-- - Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write-->
+
+// for (let t=0; t<100; t++){
+//     console.log('current step '+t+'')
+//     document.write(`<div>`)
+//     document.write('current step '+t+'')
+//     document.write(`</div>`)
+// }
+
+
+<!-- - Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write-->
+
+// for (let t=0; t<100; t+=2){
+//     console.log('current step '+t+'')
+//     document.write(`<div>`)
+//     document.write('current step '+t+'')
+//     document.write(`</div>`)
+// }
+
+
+
+<!-- - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write-->
+
+// for (let t=0; t<100; t++){
+//     if (t%2===0){
+//     console.log('current step '+t+'')
+//     document.write(`<div>`)
+//     document.write('current step '+t+'')
+//     document.write(`</div>`)}
+// }
+
+<!-- - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write-->
+
+// for (let t=0; t<100; t++){
+//     if (t%2!==0){
+//         console.log('current step '+t+'')
+//         document.write(`<div>`)
+//         document.write('current step '+t+'')
+//         document.write(`</div>`)}
+// }
