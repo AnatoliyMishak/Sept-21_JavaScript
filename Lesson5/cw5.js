5e128972ab8ffbc19827a50eeac34cd94023232e
AnatoliyMishak <mishak_tolja@ukr.net>
2021-11-23 21:24:37 +0200

    HW JS Lesson5

--- Lesson5/cw5.js
+++ Lesson5/cw5.js
@@ -11,6 +11,19 @@
 // }
 // minNum(24, 43, 11);
 
+let minNum = (num1, num2, num3) => {
+    if(num1 < num2 && num1 < num3){
+        console.log(num1)
+    }else if (num2 < num1 && num2< num3) {
+        console.log(num2)
+    }else if(num3 < num1 && num3 < num2){
+        console.log(num3)
+    }else {'Числа не можуть бути рівними'}
+}
+minNum(24, 43, 11);
+
+
+
 // - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
 // - створити функцію яка повертає найбільше число з масиву
 
@@ -26,6 +39,19 @@
 // let result = maxNumber(maxArray);
 // console.log(result);
 
+
+let maxArray = [45, 23, 56, 24, 45, 11];
+let maxNumber = (array) => {
+    let max = array[0];
+    for (let number of array) {
+        if(number > max){
+            max = number
+        }
+    }return max
+}
+let result = maxNumber(maxArray);
+console.log(result);
+
 // - створити функцію яка повертає найменьше число з масиву
 
 // let minArray = [45, 23, 56, 24, 45, 11];
@@ -40,6 +66,17 @@
 // let result = minNumber(minArray);
 // console.log(result);
 
+let minArray = [45, 23, 56, 24, 45, 13];
+let minNumber = (array) => {
+    let min = array[0];
+    for (let number of array) {
+        if(number < min){
+            min = number
+        }
+    }return min
+}
+console.log(minNumber(minArray));
+
 // - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
 
 // let summArray = [45, 23, 56, 24, 45, 11];
@@ -50,6 +87,15 @@
 //     }return total;
 // }let result = summNumber(summArray);
 // console.log(result)
+
+let summArray = [45, 23, 56, 24, 45, 11];
+let summNumber = (array) => {
+    let total = 0;
+    for (let number of array) {
+        total = total + number
+    }return total;
+}
+console.log(summNumber(summArray))
 
 // - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
 
@@ -62,6 +108,15 @@
 // }
 // let result = avrNum(avrArray);
 // console.log(result);
+
+let avrArray = [45, 23, 56, 24, 45, 11];
+let avrNum = (array) => {
+    let average = 0;
+    for (let numbers of array) {
+        average = average + numbers/array.length
+    }return average;
+}
+console.log(avrNum(avrArray));
 
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
 
@@ -79,6 +134,20 @@
 // }
 // showMinMax(array)
 
+let array4 = [22, 44, 55, 12, 65, 76, 45, 98];
+let showMinMax = (arr) => {
+let max = array4[0];
+let min = array4[0];
+    for (let number of arr) {
+        if (number > max) {
+            max = number;}
+        if (number < min) {
+            min = number; }
+        } console.log(max);
+        return min;
+}
+showMinMax(array4)
+
 // - створити функцію яка заповнює масив рандомними числами
 // (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
 
@@ -89,6 +158,13 @@
 // }
 // console.log(showRandomArray([]))
 
+showRandomArray = (arr) => {
+    for (let t = 0; t < 15; t++){
+        arr[t] = Math.round(Math.random()*100)
+    }return arr
+}
+console.log(showRandomArray([]))
+
 // - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
 
 // function showRandomArray(arr, limit){
@@ -97,6 +173,13 @@
 //     }return arr
 // }
 // console.log(showRandomArray([], 5000))
+
+let showRandomArray1 = (arr1, limit) => {
+    for (let t = 0; t < 100; t++){
+        arr1[t] = Math.round(Math.random()*limit)
+    }return arr1
+}
+console.log(showRandomArray1([], 5000))
 
 
 // - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
@@ -109,3 +192,13 @@
 //
 //     }return newArr;
 // }console.log(showRevArray(array))
+
+let array6 = [2, 3, 4, 5];
+let showRevArray = (arr) => {
+    let newArr = [];
+        for (let i = arr.length - 1, t=0; i >= 0; i--, t++) {
+        newArr[t] = arr[i];
+
+    }return newArr;
+}
+console.log(showRevArray(array6))
